757759d5fdb7801a469c590fa4f3fc378d84228d
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2024-07-25 03:10:48 +0530

    sum number with whileloop

---
+++ src/whileloop.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class whileloop {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("sum is: " + sum);
+
+
+        }
+
+    }
+
