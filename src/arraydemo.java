b72538c16ba16f82b3a72cfd71babc943f5d4357
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2024-07-24 21:42:23 +0530

    update

--- src/arraydemo.java
+++ src/arraydemo.java
@@ -1,6 +1,6 @@
 public class arraydemo {
     public static void main(String args[]){
-        String[] friendNames = {"Raj", "priyanshu", "Suraj", "David", "satyam"};
+        String[] friendNames = {"RajKumar", "priyanshu", "Suraj", "David", "satyam"};
         friendNames[3] = "ROHIT";
           for (int i = 0; i < friendNames.length; i++) {
 
