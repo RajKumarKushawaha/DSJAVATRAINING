c773ffc02e88fa93561ccd10de7525387c09d46d
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2024-07-24 22:17:19 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            if (i %2!= 0){
-               continue;
+            if (i %2== 0){
+                System.out.println("EVEN");
             }else{
-                System.out.println(i);
+                System.out.println("ODD");
             }
         }
     }
