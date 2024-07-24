572e42039950ecc6f39b940fec2fd60f353041e5
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2024-07-24 21:46:24 +0530

    update

--- src/forloop.java
+++ src/forloop.java
@@ -1,10 +1,10 @@
 public class forloop {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            if (i %2!= 0){
-                continue;
+            if (i %2== 0){
+                System.out.println("EVEN");
             }else{
-                System.out.println(i);
+                System.out.println("ODD");
             }
         }
     }
